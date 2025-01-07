e150d7e61b690a80c7ea87360636573308d8ac4f
Abdou Karim Coulibaly <98843343+absrimka@users.noreply.github.com>
2025-01-07 18:33:43 +0000

    first commit

--- config/app.php
+++ config/app.php
@@ -160,7 +160,6 @@ return [
          * Package Service Providers...
          */
         Mckenziearts\Notify\LaravelNotifyServiceProvider::class,
-        Spatie\Permission\PermissionServiceProvider::class,
         /*
          * Application Service Providers...
          */
